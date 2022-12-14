424d5794aea70d411c72c28b4a23fb76e7f3da58
SaeedullahBaloch <sp21bsse0045@maju.edu.pk>
2022-12-14 19:47:49 +0500

    Some part is done

--- src/Main/Students.java
+++ src/Main/Students.java
@@ -4,8 +4,12 @@ import java.sql.*;
 
 public class Students {
 
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	
+		
 try {
 	
 	Class.forName("com.mysql.jdbc.Driver");
@@ -14,15 +18,25 @@ try {
 	
 	Statement stmt=con.createStatement();
 	
-	ResultSet rs=stmt.executeQuery("SELECT * FROM students");
+	ResultSet rs=stmt.executeQuery("SELECT AdmissionNumber, StudentName, FatherName, DateOfBirth,Phone, Address,Email, class.Name as Class\r\n" + 
+			"FROM `students` \r\n" + 
+			"join class on class.ClassID = students.ClassesID\r\n");
 
 	
 	
 	while(rs.next())
 	{
 
-	System.out.println("ID: " + rs.getString(1) + " Name: " + rs.getString(2) + " Designation : " +rs.getString(3));
-	
+	System.out.println("Admission Number : " + rs.getString(1) +
+			" Name: " + rs.getString(2) +
+			" Father : " +rs.getString(3) + 
+			" Date of Birth : " +rs.getString(4)+
+			" Phone : " +rs.getString(5)+
+			" Address : " +rs.getString(7) +
+			" Email : " +rs.getString(6)+
+			" Class : " +rs.getString(7)
+			
+			);
 	}
 	con.close();
 		
