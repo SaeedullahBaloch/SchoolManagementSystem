5ec6af2657b2dd87cd97208f074695105531bee7
SaeedullahBaloch <sp21bsse0045@maju.edu.pk>
2023-01-01 19:41:39 +0500

    TeachersList

---
+++ src/Teachers/ListTeachers.java
@@ -0,0 +1,14 @@
+package Teachers;
+
+public class ListTeachers {
+
+	
+
+		public int TeacherID, RegistrationNumber;
+		public String Name,Father,  DateOfBirth,  Phone,  CNIC,  Qualification, Email, Address, RegistrationDate, Status;
+
+
+	
+	
+	
+}
